bcc4ffa6e415748f3f8a7ad5ab8f0c8e855e8cd4
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-03-30 20:00:43 -0300

    update variables name

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,21 @@
 import { init } from '@instantdb/admin';
 import schema from './instant.schema';
 
-if (!process.env.NEXT_PUBLIC_INSTANT_APP_ID) {
-  throw new Error('NEXT_PUBLIC_INSTANT_APP_ID is not defined in environment variables.');
+if (!process.env.INSTANT_APP_ID) {
+  throw new Error('INSTANT_APP_ID is not defined in environment variables.');
 }
 
-if (!process.env.NEXT_PUBLIC_INSTANT_API_KEY) {
-  throw new Error('NEXT_PUBLIC_INSTANT_API_KEY is not defined in environment variables.');
+if (!process.env.INSTANT_API_KEY) {
+  throw new Error('INSTANT_API_KEY is not defined in environment variables.');
 }
 
-const NEXT_PUBLIC_INSTANT_API_KEY = process.env.NEXT_PUBLIC_INSTANT_API_KEY || '';
-const NEXT_PUBLIC_INSTANT_APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID || '';
+const INSTANT_API_KEY = process.env.INSTANT_API_KEY || '';
+const INSTANT_APP_ID = process.env.INSTANT_APP_ID || '';
 
 const db = init({
-  appId: NEXT_PUBLIC_INSTANT_APP_ID,
+  appId: INSTANT_APP_ID,
   schema: schema,
-  adminToken: NEXT_PUBLIC_INSTANT_API_KEY
+  adminToken: INSTANT_API_KEY
 });
 
 export default db;
