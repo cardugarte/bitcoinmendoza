acc9f37c7902995e973ac16d4203ac1a70890cbd
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-03-30 19:41:59 -0300

    params receive in nostr

---
+++ app/api/.well-known/nostr.json/[param]/route.ts
@@ -0,0 +1,30 @@
+import { getAllNames } from '../../../../../lib/api-client';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { param: string } }
+) {
+  // Acceder al parámetro de ruta
+  const { param } = params;
+
+  const names = await getAllNames();
+
+  // Usar el parámetro para filtrar o modificar la respuesta
+  const filteredNames = names.filter(n => n.name.includes(param));
+
+  const nip05Response = {
+    names: filteredNames.reduce((acc: { [key: string]: string }, name) => {
+      acc[name.name] = name.npub;
+      return acc;
+    }, {})
+  };
+
+  return NextResponse.json(nip05Response, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
