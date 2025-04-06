23746b83308ed8eb8543c6c6765dc4eb450166f9
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-04-06 14:22:50 -0300

    params

--- app/dashboard/badge/[id]/page.tsx
+++ app/dashboard/badge/[id]/page.tsx
@@ -1,11 +1,16 @@
+import { NextPage } from 'next';
 
-const BadgePage = () => {
+type Params = {
+  id: string;
+};
+
+const BadgePage: NextPage<{ params: Params }> = async ({ params }) => {
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold text-white mb-6">Badge</h1>
+      <h1 className="text-2xl font-bold text-white mb-6">Badge #{params.id}</h1>
       {/* Contenido del badge específico */}
     </div>
-  )
-}
+  );
+};
 
-export default BadgePage; 
+export default BadgePage;
