342f0628ad555ae023a3748574e7da2a65de8b30
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-04-06 14:36:47 -0300

    fix

--- app/dashboard/badge/[id]/page.tsx
+++ app/dashboard/badge/[id]/page.tsx
@@ -1,11 +1,14 @@
-type Params = {
-  id: string;
-};
 
-export default async function BadgePage({ params }: { params: Params }) {
+
+export default async function BadgePage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold text-white mb-6">Badge #{params.id}</h1>
+      <h1 className="text-2xl font-bold text-white mb-6">Badge # {id}</h1>
       {/* Contenido del badge específico */}
     </div>
   );
--- app/dashboard/badge/claim/[id]/page.tsx
+++ app/dashboard/badge/claim/[id]/page.tsx
@@ -4,36 +4,37 @@ import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import BadgeDispenser from "@/components/ui/BadgeDispenser"
 
-type Params = {
-  id: string;
-};
-
-export default function ClaimPage({ params }: { params: Params }) {
+export default async function ClaimPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
   const router = useRouter()
 
   useEffect(() => {
     // Redirigir si no hay un ID
-    if (!params.id) {
+    if (!id) {
       router.replace("/dashboard") // Redirige al dashboard
     }
-  }, [params.id, router])
+  }, [id, router])
 
   // Asegúrate de que params.id esté disponible antes de usarlo
-  if (!params.id) {
+  if (!id) {
     return null; // O puedes mostrar un loading spinner aquí
   }
 
   const badgeData = {
-    title: `Badge #${params.id}`,
+    title: `Badge #${id}`,
     description: "Este es un badge especial que puedes reclamar.",
     imageUrl: "url_de_la_imagen_del_badge", // Reemplaza con la URL real
     tags: ["Etiqueta1", "Etiqueta2", "Etiqueta3"],
-    qrValue: "https://example.com/badge/" + params.id // URL que se codificará en el QR
+    qrValue: "https://example.com/badge/" + id // URL que se codificará en el QR
   };
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold text-white mb-6">Claim Badge #{params.id}</h1>
+      <h1 className="text-2xl font-bold text-white mb-6">Claim Badge #{id}</h1>
       <BadgeDispenser {...badgeData} />
     </div>
   )
